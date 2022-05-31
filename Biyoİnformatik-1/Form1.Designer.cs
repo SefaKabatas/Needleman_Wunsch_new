0b2c5c8d603980f3ae2e357d546d2f59eb86d176
SefaKabatas <81758761+SefaKabatas@users.noreply.github.com>
2022-05-31 14:07:32 +0300

    Add
    Add

--- Biyoİnformatik-1/Form1.Designer.cs
+++ Biyoİnformatik-1/Form1.Designer.cs
@@ -28,13 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(155, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(155, 134);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(312, 30);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(656, 39);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(137, 30);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox4.Location = new System.Drawing.Point(656, 108);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(137, 30);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox5.Location = new System.Drawing.Point(656, 180);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(137, 30);
+            this.textBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Ivory;
+            this.button1.Location = new System.Drawing.Point(817, 68);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 99);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.SeaGreen;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.InfoText;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.Color.Red;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 20);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1038, 239);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(19, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 28);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Gen1 ->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(19, 136);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Gen2 ->";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(526, 39);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Match ->";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(498, 111);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "M.Match ->";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(555, 183);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Gap ->";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.SeaGreen;
+            this.textBox6.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.ForeColor = System.Drawing.Color.Snow;
+            this.textBox6.Location = new System.Drawing.Point(390, 510);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(297, 44);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.SeaGreen;
+            this.textBox7.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.ForeColor = System.Drawing.Color.Snow;
+            this.textBox7.Location = new System.Drawing.Point(390, 564);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(297, 44);
+            this.textBox7.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Book Antiqua", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Brown;
+            this.label6.Location = new System.Drawing.Point(97, 510);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(287, 95);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Sonuç:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Snow;
+            this.label8.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(482, 569);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 32);
+            this.label8.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Snow;
+            this.label7.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(482, 515);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 32);
+            this.label7.TabIndex = 17;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 230);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1044, 262);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(1086, 631);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "180508029 SEFA KABATAŞ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Biyoİnformatik-1/Form1.cs
+++ Biyoİnformatik-1/Form1.cs
@@ -7,14 +7,163 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Biyoİnformatik_1
 {
     public partial class Form1 : Form
     {
+   
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             Belirli bir kural kümesi için diziler arasında en uygun küresel hizalamayı bulun.
+        Üç adım vardır:
+        1. Başlatma adımı
+        2. Matris doldurma adımı
+        3. Geri izleme adımı
+
+        1. Birinci dizinin uzunluğu, ikinci dizinin uzunluğu için beş değişken girin
+        sıra, eşleşme, uyumsuzluk ve boşluk.
+        2.  birinci ve ikinci dizi uzunluğu + 1 olan iki matris tanımlayın ikincisi geri izleme için.
+        3. Her iki matrisi de puan değerine göre doldurun (maksimum puan değeri opt).
+        4. Geri izleme matrisinde, köşegen için D, sol için S, üst puan için Y kullanın
+        5. Verileri GRIVİEW görünümünde görselleştirmek için her iki matrisi de veri tablosuna dönüştürün.
+        6. Her iki hizalanmış diziyi etiketlerde görüntülemek için geri izleme işlevini çağırın.
+        7. Değer D ise, o zaman her iki dizinin her iki karakterini de yazın;
+        aksi takdirde, S ve Y skor değerleri durumunda, sadece bir karakter yazıyoruz.
+        karşılık gelen dizi.
+        */
+
+            int gen_1 = textBox1.Text.Length + 1; //GEN 1
+            int gen_2 = textBox2.Text.Length + 1; //GEN2
+            if (textBox1.Text.Trim() == string.Empty || textBox2.Text.Trim() == string.Empty)//boş mu kontrol
+            {
+                MessageBox.Show("Gen1 ya da gen2 boş"); // hata mesajı göster
+
+            }
+            else //değilse
+            {
+
+                try //hata yakalama
+                {
+                    int eslesme = Convert.ToInt32(textBox3.Text); //MATCHT
+                    int esleme_olmadı = Convert.ToInt32(textBox4.Text); //DiSSMATCHT
+                    int bosluk = Convert.ToInt32(textBox5.Text); //GAP
+                    int[,] sonuc = new int[gen_1, gen_2]; //SONUCU DİZİDE TUTLA
+                    char[,] izlemeMat = new char[gen_1, gen_2]; //SONUCU YAZDIRMAYI DİZİDE TUTMA
+                    sonuc[0, 0] = 0; //DİZİ İÇİN BAŞLANGIÇ ATAMASI
+                    for (int i = 1; i < gen_1; i++) //Döngü 1 den başlıyor çünkü gen+1
+                    {
+                        sonuc[i, 0] = i * bosluk; //sonuc'a dikey kısmının boşluk ile çarpımını ekle
+                        izlemeMat[i, 0] = 'Y'; // Yukarı kısmı oluyor bu
+                    }
+                    for (int j = 1; j < gen_2; j++) //Döngü 1 den başlıyor çünkü gen2+1
+                    {
+                        sonuc[0, j] = j * bosluk; ////sonuc'a dikey kısmının boşluk ile çarpımını ekle
+                        izlemeMat[0, j] = 'S'; // Sol kısım oluyor bu
+                    }
+                    for (int i = 1; i < gen_1; i++) //i_= 1 den gen1 e kadar
+                    {
+                        for (int j = 1; j < gen_2; j++)//i=1 den gen2 ye kadar
+                        {
+                            int puanDiag = 0;
+                            if (textBox2.Text.Substring(j - 1, 1) == textBox1.Text.Substring(i - 1, 1)) //j-1 den 1 kadar al i-1 den j ye kadar al birbirine eşit ise
+                                puanDiag = sonuc[i - 1, j - 1] + eslesme; //puan'a sonuc i ve j değerlerini ve eşleşmeyi ekle
+                            else
+                                puanDiag = sonuc[i - 1, j - 1] + esleme_olmadı; //değilse eşleşme olmadıyı ekle
+                            int puanSol = sonuc[i, j - 1] + bosluk;
+                            int puanYukarı = sonuc[i - 1, j] + bosluk; //yukarı'ya sonuc un i-1 ve j kısmına boşlukla beraber ekle
+                            int maxPuan = Math.Max(Math.Max(puanDiag, puanSol), puanYukarı);
+                            //maksimum puan için puanDiag ve puan solun maxini al ve sonra çıkan sonucun puan yukarıdan hangisi yüksek ise onu al
+                            sonuc[i, j] = maxPuan; //max puanı sonuca eşitle
+                            if (sonuc[i, j] == puanDiag) //şayet sonuç puanDiag ile eşitse
+                            {
+                                izlemeMat[i, j] = 'D'; //izleme matrisi D
+                               
+
+                            }
+                            else if (sonuc[i, j] == puanSol) //sola eşitse S
+                            {
+                                izlemeMat[i, j] = 'S';
+                            }
+                            else if (sonuc[i, j] == puanYukarı) //yukarıya eşitse Y
+                            {
+                                izlemeMat[i, j] = 'Y';
+                            }
+
+                        }
+                    }
+                    dataGridView1.DataSource = DiziGridViewAtla(sonuc, gen_1, gen_2); //datagridview e ekle
+                    GeriIzleme(izlemeMat, textBox1.Text, textBox2.Text); //geri izlemeyi ekle
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Girmeyi unuttuğunuz bir değer var",ex.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //hata mesajı göster
+                }
+            }
+        }
+        public string tersYazdırma(string giris) //ters yazdırma metodu
+        {
+            char[] dizi = giris.ToCharArray();
+            Array.Reverse(dizi); //ters çevir
+            return new string(dizi); //geri döndür
+        }
+        public DataTable DiziGridViewAtla(int[,] maxSkor, int a, int b)
+        {
+            DataTable gridview = new DataTable();
+            for (int j = 0; j < b; j++)
+            {
+                gridview.Columns.Add(j.ToString()); //datagridview'e ekle
+            }
+            for (int i = 0; i < a; i++)
+            {
+                gridview.Rows.Add();
+                for (int j = 0; j < b; j++)
+                    gridview.Rows[i][j] = maxSkor[i, j]; //maxskora gridview'in Row'una ekle
+            }
+            gridview.AcceptChanges(); //değşiklikleri kaydet
+            return gridview; //geri döndür
+
+        }
+        //geri izleme metodu
+        public void GeriIzleme(char[,] geri_izleme_mat, string genA, string genB)
+        {
+            int i = geri_izleme_mat.GetLength(0) - 1;
+            int j = geri_izleme_mat.GetLength(1) - 1;
+            string hizalamaA = ""; //tanımlamalar
+            string hizalamaB = ""; //tanımlamalar
+            while (i != 0 || j != 0) //0'a eşit oluncaya kadar dön
+            {
+                switch (geri_izleme_mat[i, j])
+                {
+                    case 'D': //geri izlemede D ise
+                        hizalamaA += genA[i - 1];
+                        hizalamaB += genB[j - 1];
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Yellow;
+                        i--;
+                        j--;
+                        break;
+                    case 'Y'://geri izlemede Yukarı ise
+                        hizalamaA += genA[i - 1];
+                        hizalamaB += "-";
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Yellow;
+                        i--;
+                        break;
+                    case 'S'://geri izlemede Sol ise
+                        hizalamaA += "-";
+                        hizalamaB += genB[j - 1];
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Yellow;
+                        j--;
+                        break;
+                }
+
+            }
+            label7.Text = tersYazdırma(hizalamaA); //yazdırma
+            label8.Text = tersYazdırma(hizalamaB); //Yazdırma
         }
     }
 }
